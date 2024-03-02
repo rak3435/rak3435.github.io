0a104a39b6a804956c29a1cac2aa1b366242a927
rak3435 <152066392+rak3435@users.noreply.github.com>
2024-03-02 08:44:43 +0700

    Update hbo.js

--- hbo.js
+++ hbo.js
@@ -12,9 +12,9 @@ function paramountnetwork1() {document.getElementById("objek").innerHTML = '<ifr
 };
 function wb1() {document.getElementById("objek").innerHTML = '<iframe src="intent://ais-s.ais-vidnt.com/ais/play/origin/live/eds/X105/DASH/X105.mpd|drmScheme=clearkey&drmLicense=00a6eeb6a8623e7f5dd0de890fc55f52:57349ce17aa9f5692ad2fb7d9cdb01c7#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.referer=https://movie22hd.com/;S.title=wb;end"></iframe>'
 };
-function 3bbasian() {document.getElementById("objek").innerHTML = '<iframe src="intent://ubn-streamer1.cdn.3bbtv.com:8443/3bb/live/101/101.mpd|drmScheme=clearkey&drmLicense=dac6cbd9d17a451bb76386f52469e0e9:2e2ac52cadf843459915eaa1a9b95e48#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.referer=https://movie22hd.com/;S.title=3bbasian;end"></iframe>'
+function asian() {document.getElementById("objek").innerHTML = '<iframe src="intent://ubn-streamer1.cdn.3bbtv.com:8443/3bb/live/101/101.mpd|drmScheme=clearkey&drmLicense=dac6cbd9d17a451bb76386f52469e0e9:2e2ac52cadf843459915eaa1a9b95e48#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.referer=https://movie22hd.com/;S.title=3bbasian;end"></iframe>'
 };
-function Mono29Plus() {document.getElementById("objek").innerHTML = '<iframe src="intent://ubn-streamer1.cdn.3bbtv.com:8443/3bb/live/109/109.mpd|drmScheme=clearkey&drmLicense=072eeb02f6744f838fcc8f0ef36b312e:b797c12f2dd64728aae23fd3c8ef275c#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.referer=https://movie22hd.com/;S.title=Mono29Plus;end"></iframe>'
+function Plus() {document.getElementById("objek").innerHTML = '<iframe src="intent://ubn-streamer1.cdn.3bbtv.com:8443/3bb/live/109/109.mpd|drmScheme=clearkey&drmLicense=072eeb02f6744f838fcc8f0ef36b312e:b797c12f2dd64728aae23fd3c8ef275c#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.referer=https://movie22hd.com/;S.title=Mono29Plus;end"></iframe>'
 };
 function Rock1() {document.getElementById("objek").innerHTML = '<iframe src="intent://ais-s.ais-vidnt.com/ais/play/origin/live/eds/V0165/DASH/V0165.mpd|drmScheme=clearkey&drmLicense=3837d26526efca2fa1f18823bd073c0f:ab4d56168a618b2413beb137d70ac453#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.referer=https://movie22hd.com/;S.title=Rock-Entertainment;end"></iframe>'
 };
